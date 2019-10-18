f5c063355d07855dab417f89bc62a3a21b542eb9
Dmytro Cisar <dmytro.cisar@gmail.com>
2019-10-19 01:22:41 +0300

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,34 @@
 /* https://github.com/tailwindcss/tailwindcss/blob/master/src/corePlugins.js */
 
+const defaultVarsModules = {
+  colors: 'color',
+  screens: '',
+  fontFamily: 'font',
+  fontSize: 'text',
+  fontWeight: 'font',
+  lineHeight: 'leading',
+  letterSpacing: 'tracking',
+  backgroundSize: 'bg',
+  borderWidth: 'border',
+  borderRadius: 'rounded',
+  width: 'w',
+  height: 'h',
+  minWidth: 'min-w',
+  minHeight: 'min-h',
+  maxWidth: 'max-w',
+  maxHeight: 'max-h',
+  padding: 'p',
+  margin: 'm',
+  boxShadow: 'shadows',
+  zIndex: 'z',
+  opacity: 'opacity'
+}
+
+const disabledVarsModules = Object.keys(defaultVarsModules)
+  .reduce((o, item) => ({...o, [item]: false }), {});
+
+// console.log(disabledVarsModules);
+
 module.exports = {
   corePlugins: [
     'textColor'
@@ -14,5 +43,13 @@ module.exports = {
   variants: {
     textColor: []
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-css-variables')(
+      {
+        ...disabledVarsModules,
+        colors: 'color'
+      },
+      {postcssEachVariables:true}
+    )
+  ],
 }
